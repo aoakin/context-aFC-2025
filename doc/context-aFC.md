8768ada1ab15e864c85a4c1ff33af223b854c861
Ayomikun <ayomikun.akinrinade@seattlechildrens.org>
2025-01-27 01:34:43 -0800

    pca and document with results

---
+++ doc/context-aFC.md
@@ -0,0 +1,31 @@
+Continuous modeling of genetic regulatory variation across cellular contexts
+---
+**Authors:**\
+Ayomikun O. Akinrinade\
+Pejman Mohammadi\
+*Winter 2025*
+
+## Jan26-2025
+Task: PCA on median TPM
+
+- Top two PC axes make up ~75% of the variation in expression
+- Entirely differentiated by whether or not a tissue is blood or not
+- PC1: Top 10 eigengenes: 9 mitochondrial genes [-]  and hemoglobin subchain beta (HBB) [+]
+- PC2: HBB and HBA2,  S100A9 (inflammation + immune response), 7 mitochondrial genes [all, +]
+
+### Experiment 1 - remove blood and pancreas; all genes
+- 73% of expr. var. (PC1) primarily epxlained by mitochondrial gene expr.
+
+### Experiment 2 - all tissues; remove mitochondrial genes
+- PC1 (blood vs not blood), PC2 (pancreas vs not pancreas)
+
+### Experiment 3 - remove blood and pancreas; remove mitochondrial gene
+- High hit genes look slightly more interesting biologically?? (***TODO*** - check back on this)
+
+**PC1** (in order of eigenvector magnitude):\
+PRL/+, GH1/+, POMC/+, CGA/+, ACTB/-, LHB/+, NNAT/+, S100A9, FTL/-, DES/-
+
+**PC2**:\
+S100A9/-, KRT13/-, KRT4/-, S100A8/-, SPRR3/-, KRTS/-, KRT6A/-, CRNN/-, RHCG/-, MTATP6P1/+
+
+
--- results/2025-01-23_generate-env-axes/gen_cenv.R
+++ results/2025-01-23_generate-env-axes/gen_cenv.R
@@ -1,6 +1,148 @@
 library(ggplot2)
+library(ggfortify)
+library(data.table)
+library(corrplot)
+library(rmcorr)
 
-expr <- read.table('../../dat/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct', skip=2, sep="\t")
+expr <- read.table('../../dat/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct', skip=2, sep="\t", header=TRUE) #rows: genes, cols: tissues # nolint: line_length_linter.
+expr$Name <-  gsub("\\.\\d+$", "", expr$Name) # remove .# at the end of every gene name
+gene_descriptions <- setNames(expr$Description, expr$Name)
 
-prcomp(expr)
+# Experiment 1: remove blood and pancreas; all genes
+# expr <- subset(expr, select=-c(`Whole.Blood`, `Pancreas`))
+## Result: 73% of expression variance primarily explained by mitochondria
 
+# Experiment 2: all tissues; remove mitochondrial genes
+# expr <- expr[!grepl("^MT-", expr$Description), ]
+## Result: PC1 blood vs not blood, PC2 pancreas vs not pancreas
+
+# Experiment 3: remove blood and pancrease; remove mitochondral genes
+# expr <- subset(expr, select=-c(`Whole.Blood`, `Pancreas`))
+# expr <- expr[!grepl("^MT-", expr$Description), ]
+## Result: slighly more reasonable!
+
+# Experiment 4: remove blood, pancreas, and pituitary; remove mitochondrial genes
+expr <- subset(expr, select=-c(`Whole.Blood`, `Pancreas`, `Pituitary`))
+expr <- expr[!grepl("^MT-", expr$Description), ]
+
+
+tissue_types <- colnames(expr)[-c(1:2)]
+table(duplicated(expr$Name)) # check if any duplicates; answer: all FALSE
+t_expr <- transpose(expr)
+names(t_expr) <- t_expr[1,] # make gene IDs the column names
+t_expr <- t_expr[-(1:2),]       # remove gene id and description
+# head(t_expr)[,c(0:5)] # view first 5 row,col in expr. data
+# t_expr <- sapply(t_expr, as.numeric)
+t_expr <- as.data.frame(sapply(t_expr, as.numeric))
+head(t_expr)[,c(0:5)] # view first 5 row,col in expr. data
+# expr <- t(expr) # cols
+# expr_mat <- (t(expr[,-c(1:2)])
+pc <- prcomp(t_expr)
+str(t_expr)
+str(pc)
+
+
+n_hits <- 10
+total_pc_axes <- ncol(pc$rotation)
+
+
+hit_df_rownames <- paste0("PC", 1:total_pc_axes)
+hit_df_colnames <- paste0("hit_", 1:n_hits)
+high_hits_df <- data.frame(matrix(NA, 
+                  nrow = length(hit_df_rownames), 
+                  ncol = length(hit_df_colnames),
+                  dimnames = list(hit_df_rownames, hit_df_colnames)))
+
+# axis = 1
+for (axis in colnames(pc$rotation)) {
+  gene_eigenvec <- pc$rotation[,axis]
+  top_genes_ens_id <- names(head(sort(abs(gene_eigenvec), decreasing=TRUE), n_hits))
+  top_genes <- unname(gene_descriptions[top_genes_ens_id]) # this mess gets the top n genes
+  top_gene_eigenvec <- gene_eigenvec[top_genes_ens_id]
+  gene_eigenvec_direction <- ifelse(top_gene_eigenvec > 0, "+", "-")
+  top_genes_and_direction <- paste(top_genes, gene_eigenvec_direction, sep = "/")
+  high_hits_df[axis, ] <- top_genes_and_direction
+}
+
+# abs(pc$rotation[,1])
+
+high_hits_df['PC1',]
+high_hits_df['PC2',]
+
+
+# pc$rotation
+# top_hits_cenv1 <- tail(sort(pc$rotation[,1]), 10) # top 10 loaded genes on env xis 1 
+# # cenv 1
+# gene_descriptions[names(top_hits_cenv1)]
+
+plot(pc) # TODO figure out how to plot pc axies
+# head(t_expr)
+
+pc_eigenval = (pc$sdev)^2
+total_pc_var <- sum(pc_eigenval)
+
+
+for (i in 1:length(pc_eigenval)) {
+  cumulative_var = sum(pc_eigenval[1:i])/total_pc_var
+  cat(sprintf("Cumulative variance at PC%s: %s\n", i, cumulative_var))
+}
+
+# Cumulative variance thresholds
+# > 90.0%: PC6   [~91.38%]
+# > 95.0%: PC10  [~95.12%]
+# > 99.0%: PC20  [~99.06%]
+# > 99.7%: PC28  [~99.74%]
+
+gene_descriptions['ENSG00000210082']
+
+
+autoplot(pc, x = 1, y = 2) + theme_bw()
+
+min(pc$x[,1])
+
+pc_dat <- data.frame(pc$x)
+nrow(pc_dat) == length(tissue_types) # check if number of rows is equal to number of tissue types
+rownames(pc_dat) <- tissue_types
+
+# pc_dat
+
+# tissue_types
+
+rownames(pc_dat)[which.min(pc_dat$PC1)] 
+
+pc$x[,1]
+
+pc$x[,1]
+
+
+
+# how correlated are pancreatic and whole blood expression?
+
+gtex_corr_mat <- cor(subset(expr, select= -c(`Name`,`Description`)))
+testRes = cor.mtest(subset(expr, select= -c(`Name`,`Description`)), conf.level = 0.95)
+
+
+# gtex_rmcorr_mat <- rmcorr_mat(participant = )
+
+
+## specialized the insignificant value according to the significant level
+corrplot(gtex_corr_mat, p.mat = testRes$p, sig.level = 0.10, order = 'hclust', addrect = 2)
+
+my.colramp <- colorRampPalette(c("black", "white","brown4"))
+
+corrplot(gtex_corr_mat,
+         tl.col = 'black',
+         tl.cex=0.5, 
+         diag = TRUE, 
+         order = 'hclust', 
+         p.mat = testRes$p, 
+         sig.level = 0.10,
+         addrect = 8,
+         method = "color",
+         col = my.colramp(100))
+
+testRes$p
+
+subset(expr, select= -c(`Name`,`Description`))
+
+summary(lm(Whole.Blood ~ `Brain...Hippocampus`, data=expr))
